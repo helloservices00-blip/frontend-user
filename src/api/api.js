511f464525332e453a041550c6ccdfdda1e6f360
helloservices00-blip <helloservices00@gmail.com>
2025-11-21 17:57:40 +0530

    Create api.js

---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://backend-8xqn.onrender.com/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+// Add token to requests automatically
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  if(token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
